2035437d78512f819914b9d61885896c13821386
Avi Wasse <aviws198888@gmail.com>
2019-11-26 00:12:34 +0200

    Update servers.component.ts

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
   
   constructor() {
     setTimeout( () => { 
@@ -22,5 +23,14 @@ export class ServersComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was Craeted! ';
+  }
+
+  onUpdateServerName() {
+    
+  }
+
 
 }
