24bd21afc3a91d9d19dc1dbf216b817f578efa02
Avi Wasse <aviws198888@gmail.com>
2019-12-06 02:04:41 +0200

    Update servers.component.ts

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -16,6 +16,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'] ; 
 
   constructor() {
     setTimeout( () => { 
@@ -27,6 +29,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was Craeted! Name is : ' + this.serverName ;
   }
 
